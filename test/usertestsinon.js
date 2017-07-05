f5c2ba8b5068c997ddd1b940bedc3becd39e451e
Ravichandran Jv <ravichandranjv@users.noreply.github.com>
2017-07-06 03:36:37 +0530

    Add files via upload

---
+++ test/usertestsinon.js
@@ -0,0 +1,24 @@
+﻿var assert=require('assert');
+var request = require('supertest');
+var sinon     = require('sinon');
+var spy=sinon.spy();
+describe('simple test of about route with params',function(){
+	var server;
+	beforeEach(function () {
+  		server = require('../server').server;
+	});
+
+it ('matches the response JSON when the about page is called',function(done){
+	var getSpy=sinon.spy(server,'get');
+	const expectedJson = {"data":{"username":"hellojv"}};
+
+ 	request(server)
+    	.get('/about/jv')
+    	.expect(200)
+    	.end(function (err, response) {
+	 sinon.assert.match(response.body, expectedJson);	
+	done();
+	});
+});
+
+});
